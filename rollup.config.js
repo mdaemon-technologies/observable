a13a53415666e0dec52b66023b9a1bcb68f7663e
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-05-13 10:09:25 -0500

    Update to rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,9 @@ import typescript from "@rollup/plugin-typescript"
 export default [
   {
     input: "src/observable.ts",
-    output: {
-      name: "observable",
-      file: "dist/observable.umd.js",
-      format: "umd"
-    },
-    plugins: [
-      typescript()
-    ]
-  },
-  {
-    input: "src/observable.ts",
     output: [
+      {
+        file: "dist/observable.umd.js", format: "umd", name: "observable", exports: "default" },
       { file: "dist/observable.cjs", format: "cjs", exports: "default" },
       { file: "dist/observable.mjs", format: "es" }
     ],
